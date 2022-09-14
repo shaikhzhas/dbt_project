210acde57198cc90bd8b36f11bedb2f82056d402
Zhassulan <shaikh.zhas@gmail.com>
2022-09-14 04:53:39 +0000

    some changes

---
+++ target/compiled/dbt_project/models/example/schema.yml/not_null_my_first_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from DEMO_DB.PUBLIC.my_first_dbt_model
+where id is null
+
+
---
+++ target/compiled/dbt_project/models/example/schema.yml/not_null_my_second_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from DEMO_DB.PUBLIC.my_second_dbt_model
+where id is null
+
+
